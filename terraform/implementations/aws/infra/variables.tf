6c49296b3c89088925c7d326c3decf121e71fae3
Rakshithb12 <152890955+Rakshithb12@users.noreply.github.com>
2026-01-16 16:45:12 +0530

    Update regex for rancher_import_url validation
    Signed-off-by: Rakshithb12 <152890955+Rakshithb12@users.noreply.github.com>

--- terraform/implementations/aws/infra/variables.tf
+++ terraform/implementations/aws/infra/variables.tf
@@ -90,7 +90,7 @@ variable "rancher_import_url" {
   validation {
     condition = (
       var.rancher_import_url == "" ||
-      can(regex("^\"kubectl apply -f https://[a-zA-Z0-9][a-zA-Z0-9\\-\\.]+[a-zA-Z0-9]\\.[a-zA-Z]{2,}/v3/import/[a-zA-Z0-9_\\-]+\\.yaml\"$", var.rancher_import_url))
+      can(regex("^\"kubectl apply -f https://.+/v3/import/.+\\.yaml\"$", var.rancher_import_url))
     )
     error_message = "The rancher_import_url must be empty or in the format: '\"kubectl apply -f https://<domain>/v3/import/<ID>.yaml\"'"
   }
